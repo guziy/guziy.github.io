47a77ae43fc8bae915dd7768b2d4407ca6ae3218
guziy <guziy.sasha@gmail.com>
2014-01-30 10:19:43 -0500

    start documenting things about NEMO model

--- nemo_equations/present.sh
+++ nemo_equations/present.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ipython nbconvert nemo_equations.ipynb --to slides --post serve --config slides_config.py --template default_transition.tpl
+ipython nbconvert nemo_v3.4_equations.ipynb --to slides --post serve --config slides_config.py --template default_transition.tpl
